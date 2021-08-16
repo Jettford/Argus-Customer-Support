ed92aceb5759152c3381c9f74eb879b935ee796a
Jett <17jbradford@tqacademy.co.uk>
2021-08-16 20:24:32 +0100

    Update leaderboardHandler.js

--- modules/leaderboardHandler.js
+++ modules/leaderboardHandler.js
@@ -4,17 +4,21 @@ const {getGenericEmbed} = require("./embeds.js");
 const {MessageEmbed} = require("discord.js");
 
 async function updateLeaderboard() {
-    let leaderboardChannel = client.channels.cache.get(config["LEADERBOARD_CHANNEL"]);
-    if (leaderboardChannel.partial) await leaderboardChannel.fetch();
-    let message = await leaderboardChannel.messages.fetch(config["LEADERBOARD_MESSAGE"]);
-    const leaderboardEmbed = new MessageEmbed().setColor('#0099ff').setTitle("**Leaderboard**");
-    let descriptionValue = [];
-    db.each("SELECT * FROM threads WHERE is_locked = 0 ORDER BY (thumbs_up - thumbs_down) DESC LIMIT 10;", [], async function (err, row) {
-        descriptionValue.push(`<#${row.channel_id}> = ${(row.thumbs_up - row.thumbs_down)}`);
-    }, async () => {
-        leaderboardEmbed.setDescription(descriptionValue.join("\n"));
-        await message.edit({embeds: [leaderboardEmbed]});
-    });
+    try {
+        let leaderboardChannel = client.channels.cache.get(config["LEADERBOARD_CHANNEL"]);
+        if (leaderboardChannel.partial) await leaderboardChannel.fetch();
+        let message = await leaderboardChannel.messages.fetch(config["LEADERBOARD_MESSAGE"]);
+        const leaderboardEmbed = new MessageEmbed().setColor('#0099ff').setTitle("**Leaderboard**");
+        let descriptionValue = [];
+        db.each("SELECT * FROM threads WHERE is_locked = 0 ORDER BY (thumbs_up - thumbs_down) DESC LIMIT 10;", [], async function (err, row) {
+            descriptionValue.push(`<#${row.channel_id}> = ${(row.thumbs_up - row.thumbs_down)}`);
+        }, async () => {
+            leaderboardEmbed.setDescription(descriptionValue.join("\n"));
+            await message.edit({embeds: [leaderboardEmbed]});
+        });
+    } catch (e) {
+
+    }
 }
 
 class LeaderboardHandler extends Handler {
